594dfb8e1330e8ca3d5bcb06a24d84d2cf983963
San <San@DESKTOP-G5EOVTH>
2021-04-10 18:20:59 +0300

    Fixed tests

--- src/main/settings.js
+++ src/main/settings.js
@@ -6,7 +6,9 @@ export class Settings {
   constructor(opts) {
     // Renderer process has to get `app` module via `remote`, whereas the main process can get it directly
     // app.getPath('userData') will return a string of the user's app data directory path.
-    const userDataPath = (electron.app || electron.remote.app).getPath('userData');
+    // SAG: this fails in jest
+    // const userDataPath = electron.app || electron.remote.app).getPath('userData');
+    const userDataPath = process.env.APPDATA + '\\staker';
     // We'll use the `configName` property to set the file name and path.join to bring it all together as a string
     this.path = path.join(userDataPath, opts.configName + '.json');
     
