b5fcf0696097991da33d67789216dcd19c26b232
San <sganis@gmail.com>
2021-04-19 16:01:23 +0300

    Created install script for services

--- tool/systemd.sh
+++ tool/systemd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo bash -c "cat << 'EOF' > /etc/systemd/system/cardano.service
+sudo bash -c "cat << 'EOF' > /etc/systemd/system/cardano-node.service
 [Unit]
 Description=Cardano Node
 After=network.target
@@ -17,7 +17,7 @@ KillSignal=SIGINT
 SuccessExitStatus=143
 StandardOutput=syslog
 StandardError=syslog
-SyslogIdentifier=cardano
+SyslogIdentifier=cardano-node
 TimeoutStopSec=5
 KillMode=mixed
 
@@ -27,3 +27,32 @@ EOF"
 
 sudo systemctl daemon-reload
 sudo systemctl enable cardano.service
+
+sudo bash -c "cat << 'EOF' > /etc/systemd/system/cardano-wallet.service
+[Unit]
+Description=Cardano Wallet
+After=network.target
+
+[Service]
+Type=simple
+Restart=on-failure
+RestartSec=5
+User=$USER
+LimitNOFILE=1048576
+WorkingDirectory=$HOME/cardano/relay
+ExecStart=/bin/bash -l -c \"exec $HOME/cardano/bin/start_wallet.sh\"
+ExecStop=/bin/bash -l -c \"exec kill -2 \$(ps -ef | grep [c]ardano-wallet | tr -s ' ' | cut -d ' ' -f2)\"
+KillSignal=SIGINT
+SuccessExitStatus=143
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=cardano-wallet
+TimeoutStopSec=5
+KillMode=mixed
+
+[Install]
+WantedBy=multi-user.target
+EOF"
+
+sudo systemctl daemon-reload
+sudo systemctl enable cardano-wallet.service
