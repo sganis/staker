fc385ba6c7471705e257c693fa346ffff2f3ff59
San <sganis@gmail.com>
2021-04-26 13:17:12 +0300

    Updated install tools

--- tool/bin/install.sh
+++ tool/bin/install.sh
@@ -6,11 +6,13 @@ ROOT=$HOME/cardano
 
 mkdir -p $ROOT
 mkdir -p $ROOT/keys
-mkdir -p $ROOT/bin
-mkdir -p $ROOT/config
 
-cp -r $DIR/* $ROOT/bin
-cp -r $DIR/../config/* $ROOT/config
+if [[  "$DIR" != "$ROOT/bin" ]]; then
+	mkdir -p $ROOT/bin
+	cp -r $DIR/* $ROOT/bin
+	mkdir -p $ROOT/config
+	cp -r $DIR/../config/* $ROOT/config
+fi
 
 $DIR/systemd.sh
 
