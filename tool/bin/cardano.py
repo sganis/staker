27135657230483a7df0097db618a0ed5a13da2ba
San <sganis@gmail.com>
2021-05-02 23:25:28 +0300

    Working on registration script

--- tool/bin/cardano.py
+++ tool/bin/cardano.py
@@ -14,26 +14,30 @@ DIR = os.path.dirname(os.path.abspath(sys.argv[0]))
 ROOT=f'{home}/cardano'
 CONF=f'{ROOT}/config'
 KEYS = f'{ROOT}/keys'
+WALLETS = f'{ROOT}/wallets'
 KEYFILES = ['cold.skey','cold.vkey','cold.counter','vrf.skey','vrf.vkey','kes.skey','kes.vkey','node.cert']
 NETWORK='--testnet-magic 1097911063'
 # os.environ["CARDANO_NODE_SOCKET_PATH"] = path_to_socket
 
 def run(cmd):
 	cmd = cmd.replace('\n','')
-	#print(f'CMD: {cmd}')
+	# print(f'CMD: {cmd}')
 	p = subprocess.run(cmd.replace('\n','').split(), capture_output=True)
 	stdout = p.stdout.decode().strip()
 	stderr = p.stderr.decode().strip()
 	return stdout, stderr
 
 def _get_protocol():
-	cmd = f'''cardano-cli query protocol-parameters --out-file protocol.json {NETWORK} --mary-era'''
-	# print(cmd)
-	run(cmd)
+	cmd = f'{DIR}/cardano-cli query protocol-parameters --out-file {CONF}/protocol.json {NETWORK}'
+	o,e = run(cmd)
+	if e:
+		print(e, file=sys.stderr)
 
 def _get_utxo(addr):
-	cmd = f'''cardano-cli query utxo --address {addr} {NETWORK} --mary-era'''
+	cmd = f'''cardano-cli query utxo --address {addr} {NETWORK}'''
 	o,e = run(cmd)
+	if e:
+		print(e, file=sys.stderr)
 	# print(o)
 	lines = o.split('\n')[2:]
 	utxo = []
@@ -45,33 +49,35 @@ def _get_utxo(addr):
 	return utxo
 
 def _calculate_min_fee(tx_in_count, tx_out_count, witness_count):
-	cmd = 'cardano-cli transaction calculate-min-fee '
-	cmd += f'--tx-body-file tx.draft '
+	cmd = f'{DIR}/cardano-cli transaction calculate-min-fee '
+	cmd += f'--tx-body-file {KEYS}/tx.draft '
 	cmd += f'--tx-in-count {tx_in_count} '
 	cmd += f'--tx-out-count {tx_out_count} '
 	cmd += f'--witness-count {witness_count} '
-	cmd += '--byron-witness-count 0 --protocol-params-file protocol.json '
+	cmd += f'--byron-witness-count 0 --protocol-params-file {CONF}/protocol.json '
 	cmd += f'{NETWORK}'
+	print(cmd)
 	o,e = run(cmd)
 	return int(o.split()[0])
 
 def _get_tip_slot_number():
-	cmd  = f'cardano-cli query tip {NETWORK}'
+	cmd  = f'{DIR}/cardano-cli query tip {NETWORK}'
 	o, e = run(cmd)
 	js = json.loads(o)
-	return int(js['slotNo'])
+	# print(js)
+	return int(js['slot'])
 
 def _get_ttl():
 	# ttl
 	return _get_tip_slot_number() + 200
 
 def _get_key_deposit():
-	js = json.loads(open('protocol.json').read())
+	js = json.loads(open(f'{CONF}/protocol.json').read())
 	return int(js['keyDeposit'])
 
 def _get_pool_deposit():
-	js = json.loads(open('protocol.json').read())
-	return int(js['poolDeposit'])
+	js = json.loads(open(f'{CONF}/protocol.json').read())
+	return int(js['stakePoolDeposit'])
 
 def _get_args():
 	parser = argparse.ArgumentParser()
@@ -108,6 +114,12 @@ def _get_args():
 
 	s = subparsers.add_parser("stake-address-info", help='get stake address info')
 	s.add_argument("--address", required=True, help="stake address")
+
+	s = subparsers.add_parser("register-pool", help='register pool')
+	s.add_argument("--pledge", required=True, help="pledge in ADA")
+	s.add_argument("--margin", required=True, help="margin percent")
+	s.add_argument("--cost", required=True, help="cost in ADA")
+	s.add_argument("--wallet-id", required=True, help="wallet id of payment and stake")
 
 	
 	# Parse
@@ -175,7 +187,7 @@ def send(from_addr, to_addr, ada, from_skey):
 		if balance > min_input:
 			break
 
-	cmd = 'cardano-cli transaction build-raw '
+	cmd = f'{DIR}/cardano-cli transaction build-raw '
 	cmd += f'{tx_in}'
 	cmd += f'--tx-out {to_addr}+0 '
 	cmd += f'--tx-out {from_addr}+0 '
@@ -367,84 +379,133 @@ def _get_metadata_hash():
 		return ''
 
 def _get_metadata_url():
-	o,e = run(f'cat {CONF}/pool-metatada.url')
+	o,e = run(f'cat {CONF}/pool-metadata.url')
 	if e:
 		print(e, file=sys.stderr)
 	return o
 
-def register_pool(pledge, margin, cost, cold_vkey, vrf_vkey):
+def register_pool(pledge, margin, cost, wallet_id):
 
-	payment_addr = open('payment.addr').read()
-	payment_skey = open('payment.skey').read()
-
+	WALLET = f'{WALLETS}/{wallet_id}'
+	payment_addr = open(f'{WALLET}/payment.addr').read()
 	metadata_hash = _get_metadata_hash()
-	pledge = pledge * 1_000_000
-	cost = cost * 1_000_000
+	pledge = int(pledge) * 1_000_000
+	cost = int(cost) * 1_000_000
 	metadata_url = _get_metadata_url()
 	relay_dns = "adapool.chaintrust.com"
 
-	cmd = 'cardano-cli stake-pool registration-certificate '
+	_get_protocol()
+	deposit = _get_pool_deposit()
+	# print(f'deposit: {deposit}')
+
+
+	cmd = f'{DIR}/cardano-cli stake-pool registration-certificate '
 	cmd += f'--cold-verification-key-file {KEYS}/cold.vkey '
 	cmd += f'--vrf-verification-key-file {KEYS}/vrf.vkey '
 	cmd += f'--pool-pledge {pledge} '
 	cmd += f'--pool-cost {cost} '
 	cmd += f'--pool-margin {margin} '
-	cmd += '--pool-reward-account-verification-key-file stake.vkey '
-	cmd += '--pool-owner-stake-verification-key-file stake.vkey '
+	cmd += f'--pool-reward-account-verification-key-file {WALLET}/stake.vkey '
+	cmd += f'--pool-owner-stake-verification-key-file {WALLET}/stake.vkey '
 	cmd += f'--single-host-pool-relay {relay_dns} '
 	cmd += '--pool-relay-port 3001 '
 	cmd += f'--metadata-url {metadata_url} '
 	cmd += f'--metadata-hash {metadata_hash} '
-	cmd += f'--out-file pool-registration.cert {NETWORK}'
+	cmd += f'--out-file {KEYS}/pool-registration.cert {NETWORK}'
+	print(cmd)
+	print()
 	o,e = run(cmd)
+	if e:
+		print(e, file=sys.stderr)
+		return False
 
-	cmd = '''cardano-cli stake-address delegation-certificate 
-		--stake-verification-key-file stake.vkey 
-		--cold-verification-key-file cold.vkey 
-		--out-file delegation.cert'''
-	run(cmd)
+	# delegation certificates for each owner
+	cmd = f'{DIR}/cardano-cli stake-address delegation-certificate '
+	cmd += f'--stake-verification-key-file {WALLET}/stake.vkey '
+	cmd += f'--cold-verification-key-file {KEYS}/cold.vkey '
+	cmd += f'--out-file {KEYS}/delegation.cert'
+	print(cmd)
+	print()
+	o,e = run(cmd)
+	if e:
+		print(e, file=sys.stderr)
+		return False
 
+	balance = 0
+	utxo = _get_utxo(payment_addr)
+	print(utxo)
+	tx_in = ''
+	tx_in_count = 0
 
-	_get_protocol()
-	txhash, txtx, balance = _get_tx_hash(payment_addr)
+	for u in utxo:
+		txhash = u[0]
+		idx = u[1]
+		tx_in += f'--tx-in {txhash}#{idx} '
+		tx_in_count += 1
+		balance += int(u[2])
+	
 
-	cmd = 'cardano-cli transaction build-raw '
-	cmd += f'--tx-in {txhash}#{txtx} '
+	cmd = f'{DIR}/cardano-cli transaction build-raw '
+	cmd += f'{tx_in}'
 	cmd += f'--tx-out {payment_addr}+0 '
-	cmd += '--invalid-hereafter 0 --fee 0 --out-file tx.draft '
-	cmd += '--certificate-file pool-registration.cert '
-	cmd += '--certificate-file delegation.cert'
-	run(cmd)
+	cmd += f'--invalid-hereafter 0 --fee 0 --out-file {KEYS}/tx.draft '
+	cmd += f'--certificate-file {KEYS}/pool-registration.cert '
+	cmd += f'--certificate-file {KEYS}/delegation.cert'
+	print(cmd)
+	print()
+	o,e = run(cmd)
+	if e:
+		print(e, file=sys.stderr)
+		return False
 
-	fee = _calculate_min_fee(1, 3)
-	print(f'fee: {fee}')
-	deposit = _get_pool_deposit()
-	print(f'deposit: {deposit}')
+	fee = _calculate_min_fee(tx_in_count, 1, 3)
+	
+	is_registered = True
+	if is_registered:
+		deposit = 0
 
 	change = balance - fee - deposit
+	print(f'balance: {balance}')
+	print(f'deposit: {deposit}')
+	print(f'fee    : {fee}')
+	print(f'change : {change}')
 
 	ttl = _get_ttl()
 
-	cmd = 'cardano-cli transaction build-raw '
-	cmd += f'--tx-in {txhash}#{txtx} '
+	cmd = f'{DIR}/cardano-cli transaction build-raw '
+	cmd += f'{tx_in}'
 	cmd += f'--tx-out {payment_addr}+{change} '
 	cmd += f'--invalid-hereafter {ttl} '
-	cmd += f'--fee {fee} --out-file tx.raw '
-	cmd += '--certificate-file pool-registration.cert '
-	cmd += '--certificate-file delegation.cert'
+	cmd += f'--fee {fee} --out-file {KEYS}/tx.raw '
+	cmd += f'--certificate-file {KEYS}/pool-registration.cert '
+	cmd += f'--certificate-file {KEYS}/delegation.cert'
+	print(cmd)
+	print()
 	o,e = run(cmd)
-
-	cmd = 'cardano-cli transaction sign --tx-body-file tx.raw '
-	cmd += '--signing-key-file payment.skey '
-	cmd += '--signing-key-file stake.skey '
-	cmd += '--signing-key-file cold.skey '
-	cmd += f'--out-file tx.signed {NETWORK}'
-	o,e = run(cmd)
-
-	o,e = run(f'cardano-cli transaction submit --tx-file tx.signed {NETWORK}')
-	print(o)
 	if e:
-		print(f'error: {e}')
+		print(e, file=sys.stderr)
+		return False
+
+	cmd = f'{DIR}/cardano-cli transaction sign --tx-body-file {KEYS}/tx.raw '
+	cmd += f'--signing-key-file {WALLET}/payment.skey '
+	cmd += f'--signing-key-file {WALLET}/stake.skey '
+	cmd += f'--signing-key-file {KEYS}/cold.skey '
+	cmd += f'--out-file {KEYS}/tx.signed {NETWORK}'
+	print(cmd)
+	print()
+	o,e = run(cmd)
+	if e:
+		print(e, file=sys.stderr)
+		return False
+	cmd = f'{DIR}/cardano-cli transaction submit --tx-file {KEYS}/tx.signed {NETWORK}'
+	print(cmd)
+	print()
+	
+	o,e = run(cmd)
+	if e:
+		print(e, file=sys.stderr)
+		return False
+	return True
 
 def _get_pool_id():
 	o,e = run(f'cardano-cli stake-pool id --cold-verification-key-file {KEYS}/cold.vkey --output-format hex')
@@ -529,6 +590,9 @@ if __name__ == '__main__':
 
 	elif p.command == 'stake-address-info':
 		ok = query_stake_address_info(p.address)
+
+	elif p.command == 'register-pool':
+		ok = register_pool(p.pledge, p.margin, p.cost, p.wallet_id)
 
 
 
